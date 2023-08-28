418537baaa7e0d8391e2cfd15c8f36ea0d533335
khulan <khulantsogt10@gmail.com>
2023-08-28 19:00:55 +0800

    khulan added 2 new files

---
+++ app.js
@@ -0,0 +1,40 @@
+console.log("ene bol console");
+var name = "Khulan";
+console.log("Minii ner "+name+" gedeg.");
+var age;
+age = 14;
+nas = 13;
+console.log("Your age is "+ nas);
+var isTrue = true;
+console.log("Minii neriig "+name+" gedeg.");
+console.log("Bi "+nas+" nastai.")
+var gazar;
+gazar = "Plekhanoviin ih suurguulid";
+console.log("Bi "+gazar+" surdag");
+console.log("Bi emegtei gedeg ni "+true);
+var on;
+on = 2010;
+console.log("Ta "+on+ " ond tursun.")
+var too;
+console.log(too);
+var number = null;
+console.log(number);
+// typeof --> ugugdliin turliig shalgah function
+console.log(typeof name);
+console.log(typeof age);
+// Math.floor(3.14) --> 3 butarhai toog buhel too ruu huvirgah
+console.log(Math.floor(521.25643546));
+console.log("Undaanii une :"+ (2000 +500));
+// Ta <.....> ond tursun.
+var birthYear, nowAge = 13, currentYear = 2023;
+birthYear = currentYear - nowAge;
+console.log("Tanii tursun on bol " + birthYear);
+// Tanii 10 jiliin daraah nas : <.....>
+console.log("Tanii 10 jiliin daraah nas : " + (nowAge + 10));
+// git --> programm suulgah
+// github.com --> deer account uusgeh
+var number;
+number = 3;
+num = 4;
+too = 5;
+console.log(number+num+too);
